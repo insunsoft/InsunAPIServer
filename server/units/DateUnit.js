2a138d84c9be7407d771d9477d4bb11e226f8726
insunsoft <951241056@qq.com>
2019-06-13 20:40:51 +0800

    Update DateUnit.js

--- server/units/DateUnit.js
+++ server/units/DateUnit.js
@@ -1,106 +1,86 @@
 'use strict';
-//所有有关时间处理的函数放置于此。
-
-
-const   WEEKTYPE = {
-        ZH_DAYNAME: 0,
-        ZH_SDAYNAME: 1,
-        US_DAYNAME: 2,
-        US_SDAYNAME: 3,
-    };
-    const    _options = {
-        ZH: {
-            dayNames: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
-            shortDayNames: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
-            monthNames: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
-            shortMonthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
-        },
-        US: {
-            dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-            shortDayNames: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-            monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-            shortMonthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        }
-    };
+// +----------------------------------------------------------------------
+// | 项目：InsunAPIServer
+// | 版权：Copyright (c) 1974~2019 http://insunsoft.com All rights reserved.
+// | 授权：Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// | 作者: insunsoft-濮堂.陈剑 <951241056@QQ.com>
+// +----------------------------------------------------------------------
+// | 用途: 所有有关时间处理的函数放置于此。
+// | 路径: ./units/DateUnit.js
+// | 备注：已完成 
+// +----------------------------------------------------------------------
+const WEEKTYPE = {
+    ZH_DAYNAME: 0,
+    ZH_SDAYNAME: 1,
+    US_DAYNAME: 2,
+    US_SDAYNAME: 3,
+};
+const _options = {
+    ZH: {
+        dayNames: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+        shortDayNames: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
+        monthNames: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+        shortMonthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+    },
+    US: {
+        dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        shortDayNames: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        shortMonthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    }
+};
 
 module.exports = {
 
-/** 
- * 具体方法
-  getTimeAgo: 计算给定时间距离现在的时间
-  
-  1. format(date, fmt)
-  
-  format 是将 Date类型的时间进行格式化的工具方法：
-  
-  DateFormat.format(new Date()) //不传 fmt，则默认为 yyyy-MM-dd hh:mm:ss ;
-  DateFormat.format(new Date(), 'yyyy-MM-dd hh:mm:ss');
-  DateFormat.format(new Date(), 'yyyy/MM/dd hh:mm:ss');
-  DateFormat.format(new Date(), 'yyyy/MM/dd');
-  ...
-  2. formatToDate(dateStr)
-  
-  formatToDate 是将 字符串类型的时间 转化成 Date 类型的工具方法：
-  
-  DateFormat.formatToDate('2017-04-18 12:12:12');
-  DateFormat.formatToDate('2017/04/18 12:12:12');
-  ...
-  3. getDateStart(date)
-  
-  getDateStart 是得到一天的开始，工作中会碰到这种需要得到某一天的开始或结束的时间点。
-  
-  DateFormat.getDateStart(new Date());  //今天是4.18, 那返回的是今天 00:00:00的Date类型
-  如果想返回字符串类型，可以使用 getDateStartStr(date, fmt) 方法，fmt非必传
-  
-  DateFormat.getDateStartStr(new Date(), 'yyyy-MM-dd hh:mm:ss'); //返回 2017-04-18 00:00:00
-  4. getDateEnd(date)
-  
-  getDateEnd 是得到一天的结束
-  
-  DateFormat.getDateEnd(new Date());  //今天是4.18, 那返回的是今天 23:59:59的Date类型
-  如果想返回字符串类型，可以使用 getDateEndStr(date, fmt) 方法，fmt非必传
-  
-  DateFormat.getDateEndStr(new Date(), 'yyyy-MM-dd hh:mm:ss'); //返回 2017-04-18 23:59:59
-  5. compareDate(d1, d2)
-  
-  compareDate 比较两个日期的大小 返回 1 , 则 d1 > d2 返回 0 , 则 d1 == d2 返回 -1, 则 d1 < d2
-  
-  DateFormat.compareDate(new Date(), new Date()); 
-  6. getWeek(date, type)
-  
-  getWeek 得到一个日期是星期几
-  
-  DateFormat.getWeek(new Date());  //type非必传，默认返回 '星期 X'的格式
-  DateFormat.getWeek(new Date(), DateFormat.WEEKTYPE.US_DAYNAME); // Monday
-  
-  
-  
-  type 枚举如下：
-  
-  DateFormat.WEEKTYPE.ZH_DAYNAME :      星期一
-  DateFormat.WEEKTYPE.ZH_SHORTDAYNAME:  周一
-  DateFormat.WEEKTYPE.US_DAYNAME：      Monday
-  DateFormat.WEEKTYPE.US_SDAYNAME：     Mon
-  
-  7. addDay(date, num)
-  
-  增加系列：
-  
-  addDay(date, num) 
-  addDayStr(dateStr, num) 
-  addMonth(date, num)
-  addMonthStr(dateStr, num)
-  addYear(date, num)
-  addYearStr(dateStr, num)
-  
-  使用起来很简单，今天是 2017.04.18
-  
-  DateFormat.addDay(new Date(), 3);   //返回是三天后的  Date类型
-  DateFormat.addDayStr('2017-04-18 12:12:12', 3) // 返回 Fri Apr 21 2017 12:12:12 GMT+0800 (中国标准时间)
-  
-  其他 month year的方式同上
-       
-       */
+    /** 
+     * 具体方法
+      1.getTimeAgo: 计算给定时间距离现在的时间
+      
+      2. format(date, fmt)
+     format 是将 Date类型的时间进行格式化的工具方法：
+     DateUnit.format(new Date()) //不传 fmt，则默认为 yyyy-MM-dd hh:mm:ss ;
+     DateUnit.format(new Date(), 'yyyy-MM-dd hh:mm:ss');
+     DateUnit.format(new Date(), 'yyyy/MM/dd hh:mm:ss');
+     DateUnit.format(new Date(), 'yyyy/MM/dd');
+      3. formatToDate(dateStr)
+     formatToDate 是将 字符串类型的时间 转化成 Date 类型的工具方法：
+     DateUnit.formatToDate('2017-04-18 12:12:12');
+     DateUnit.formatToDate('2017/04/18 12:12:12');
+      4. getDateStart(date)
+      getDateStart 是得到一天的开始，工作中会碰到这种需要得到某一天的开始或结束的时间点。
+     DateUnit.getDateStart(new Date());  //今天是4.18, 那返回的是今天 00:00:00的Date类型
+      如果想返回字符串类型，可以使用 getDateStartStr(date, fmt) 方法，fmt非必传
+     DateUnit.getDateStartStr(new Date(), 'yyyy-MM-dd hh:mm:ss'); //返回 2017-04-18 00:00:00
+      5. getDateEnd(date)
+      getDateEnd 是得到一天的结束
+     DateUnit.getDateEnd(new Date());  //今天是4.18, 那返回的是今天 23:59:59的Date类型
+      如果想返回字符串类型，可以使用 getDateEndStr(date, fmt) 方法，fmt非必传
+     DateUnit.getDateEndStr(new Date(), 'yyyy-MM-dd hh:mm:ss'); //返回 2017-04-18 23:59:59
+      6. compareDate(d1, d2)
+      compareDate 比较两个日期的大小 返回 1 , 则 d1 > d2 返回 0 , 则 d1 == d2 返回 -1, 则 d1 < d2
+     DateUnit.compareDate(new Date(), new Date()); 
+      7. getWeek(date, type)
+      getWeek 得到一个日期是星期几
+     DateUnit.getWeek(new Date());  //type非必传，默认返回 '星期 X'的格式
+     DateUnit.getWeek(new Date(),DateUnit.WEEKTYPE.US_DAYNAME); // Monday
+      type 枚举如下：
+     DateUnit.WEEKTYPE.ZH_DAYNAME :      星期一
+     DateUnit.WEEKTYPE.ZH_SHORTDAYNAME:  周一
+     DateUnit.WEEKTYPE.US_DAYNAME：      Monday
+     DateUnit.WEEKTYPE.US_SDAYNAME：     Mon
+      8. addDay(date, num)
+      增加系列：
+      addDay(date, num) 
+      addDayStr(dateStr, num) 
+      addMonth(date, num)
+      addMonthStr(dateStr, num)
+      addYear(date, num)
+      addYearStr(dateStr, num)
+      使用起来很简单，今天是 2017.04.18
+     DateUnit.addDay(new Date(), 3);   //返回是三天后的  Date类型
+     DateUnit.addDayStr('2017-04-18 12:12:12', 3) // 返回 Fri Apr 21 2017 12:12:12 GMT+0800 (中国标准时间)
+      其他 month year的方式同上
+    */
     getTimeAgo: function (time) {
         if (!time instanceof Date) {
             time = new Date(time);
@@ -305,8 +285,6 @@ module.exports = {
             return dateStr;
         }
     }
-
-
 }
 
 
