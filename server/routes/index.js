4e86cb2c234aaeda9d0b5da400f987e8b48b5ef7
insunsoft <951241056@qq.com>
2019-06-13 08:37:15 +0800

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,18 @@
+// +----------------------------------------------------------------------
+// | InsunAPIServer
+// | Copyright (c) 1974~2019 http://insunsoft.com All rights reserved.
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// | Author: insunsoft-濮堂.陈剑 <951241056@QQ.com>
+// +----------------------------------------------------------------------
+
+// +----------------------------------------------------------------------
+// | 此处用于服务器根地址的访问接口，默认显示欢迎页面。 后期改造成admin模块入口
+// |
+// | 
+// | 
+// +----------------------------------------------------------------------
 const router = require('koa-router')()
-//const config = require('./../config')
 
-
-// 
-// 
-// 这里考虑提供Admin
 router.get('/', function (ctx, next) {
     ctx.body = returnJson(0,'Hello Wolrd!',{})
 })
