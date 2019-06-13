2df6bdea511aa584a3665b678431415ed133e22f
insunsoft <951241056@qq.com>
2019-06-13 18:31:43 +0800

    API调用返回数据方法的封装

--- server/units/ReturnUnit.js
+++ server/units/ReturnUnit.js
@@ -1,6 +1,17 @@
 'use strict';
+'use strict';
+// +----------------------------------------------------------------------
+// | 项目：InsunAPIServer
+// | 版权：Copyright (c) 1974~2019 http://insunsoft.com All rights reserved.
+// | 授权：Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// | 作者: insunsoft-濮堂.陈剑 <951241056@QQ.com>
+// +----------------------------------------------------------------------
+// | 用途: API调用返回数据方法的封装
+// | 路径: ./units/ReturnUnit.js
+// | 备注：已完成 
+// +----------------------------------------------------------------------
 module.exports = {
-
+    //成功模式
     returnSuccessJson: function (msg, data) {
         return {
             code: 'Success',
@@ -8,6 +19,7 @@ module.exports = {
             data: data
         };
     },
+    //服务器端出现错误
     returnErrorJson: function (msg, data) {
         return {
             code: 'Error',
@@ -15,13 +27,14 @@ module.exports = {
             data: data
         };
     },
+    //客户交互模式出现错误、提示
     returnInfoJson: function (msg, data) {
         return {
             code: 'Info',
             msg: msg,
             data: data
         };
-    },
+    }
 }
 
 
